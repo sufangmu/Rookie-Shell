c5628ec98dc8e5820790497927e7905dfde13b57
yuanzhi <42198815+yuanzhigao@users.noreply.github.com>
2019-11-14 11:38:48 +0800

    Update log.sh

--- script/log.sh
+++ script/log.sh
@@ -20,3 +20,37 @@ log(){
     [ $LEVEL -ge 2 ] && [ $level = "WORN" ] &&  echo ${message}
     [ $LEVEL -ge 1 ] && [ $level = "ERROR" ]  &&  echo ${message}
 }
+
+#!/bin/bash
+
+# Copyright Huawei Technologies Co., Ltd. 2017-2019. All rights reserved.
+# Des: Print log on terminal and log file
+
+set -e
+
+#debug:0; info:1; warn:2; error:3
+LOG_LEVEL=0 
+
+log()
+{
+	local msg_cont=""
+	local type=""
+
+	type="$1"
+	msg_cont="$2"
+	date_time=$(date +'%F %H:%M:%S')
+
+	log_format="${date_time} ${type} funcname: ${FUNCNAME[@]/log/} [line:$(caller 0 | awk '{print$1}')] ${msg_cont}"
+	{
+	case "${type}" in  
+		debug)
+			[[ "${LOG_LEVEL}" -le 0 ]] && echo -e "${log_format}" ;;
+		info)
+			[[ "${LOG_LEVEL}" -le 1 ]] && echo -e "${log_format}" ;;
+		warn)
+			[[ "${LOG_LEVEL}" -le 2 ]] && echo -e "${log_format}" ;;
+		error)
+			[[ "${LOG_LEVEL}" -le 3 ]] && echo -e "${log_format}" ;;
+	esac
+	} | tee -a "${NSTACK_BUILD_LOG_PATH}/build.log"
+}
